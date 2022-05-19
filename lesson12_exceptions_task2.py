3dee7f26a9bd7dc593aa4db53b1eb684a5170864
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-19 11:54:48 +0800

    Дополнил функцию discounted

--- lesson12_exceptions_task2.py
+++ lesson12_exceptions_task2.py
@@ -8,10 +8,22 @@
 '''
 
 
-def discounted(price, discount, max_discount=20):
+def discounted(price: float, discount: float, max_discount: int =20):
+
+    try:
+
+        price = float(price)
+        discount = float(discount)
+        max_discount = int(max_discount)
+
+    except ValueError as err:
+        print(err, ': Введены некорректные аргументы')
+        exit()
+
     price = abs(price)
     discount = abs(discount)
     max_discount = abs(max_discount)
+
     if max_discount >= 100:
         raise ValueError('Слишком большая максимальная скидка')
     if discount >= max_discount:
