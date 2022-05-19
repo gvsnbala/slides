e1920a1c90acaa23c1b87fa1f5cf28542fcf0dc5
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-19 10:38:29 +0800

    Вызвал и вывел значение

--- price.py
+++ price.py
@@ -7,9 +7,18 @@
     * Выведите значение переменной с результатом на экран
 '''
 
+def format_price(price) -> str:
+    try:
+        value = int(price)
+    except ValueError as err:
+        print(err)
+
+    return f'Цена: {value} руб.'
+
 
 def main():
-    pass
+    price = format_price(56.24)
+    print(price)
 
 
 if __name__ == "__main__":
