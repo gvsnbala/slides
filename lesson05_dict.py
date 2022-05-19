42f19228a2fad7bd976464ab6a808bc7f3bbb306
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-19 20:35:28 +0800

    rename dict_ -> cities

--- lesson05_dict.py
+++ lesson05_dict.py
@@ -15,15 +15,15 @@
 
 
 def main():
-    dict_ = {'city': 'Москва', 'temperature': '20'}
-    print(dict_['city'])
-    dict_['temperature'] = str(int(dict_['temperature']) - 5)
-    print(dict_)
+    cities = {'city': 'Москва', 'temperature': '20'}
+    print(cities['city'])
+    cities['temperature'] = str(int(cities['temperature']) - 5)
+    print(cities)
 
-    print('country' in dict_.keys())
-    print(dict_.get('country', 'Россия'))
-    dict_['date'] = '27.05.2019'
-    print(len(dict_))
+    print('country' in cities.keys())
+    print(cities.get('country', 'Россия'))
+    cities['date'] = '27.05.2019'
+    print(len(cities))
 
 
 if __name__ == "__main__":
